91a0bf5a506c79cf8bd5e935a0fc5cd0d3e8b30d
mustafakamber <mustkmber@gmail.com>
2024-03-19 16:32:15 +0300

    feat : space deleted

--- task-2-solution/SecondTaskSolution/app/src/main/java/com/example/secondtasksolution/util/CallBackHandler.kt
+++ task-2-solution/SecondTaskSolution/app/src/main/java/com/example/secondtasksolution/util/CallBackHandler.kt
@@ -3,7 +3,6 @@ package com.example.secondtasksolution.util
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.FragmentActivity
 
-
 object CallBackHandler {
     fun handleCallback(activity: FragmentActivity, backButtonPressed: () -> Unit) {
         val callback = object : OnBackPressedCallback(true) {
