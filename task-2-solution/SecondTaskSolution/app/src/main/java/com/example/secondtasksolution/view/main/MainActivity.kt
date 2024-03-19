b19aa550571643107063f9a735f24f3eb682cc49
mustafakamber <mustkmber@gmail.com>
2024-03-19 16:27:59 +0300

    feat : spaces added

--- task-2-solution/SecondTaskSolution/app/src/main/java/com/example/secondtasksolution/view/main/MainActivity.kt
+++ task-2-solution/SecondTaskSolution/app/src/main/java/com/example/secondtasksolution/view/main/MainActivity.kt
@@ -11,6 +11,7 @@ import com.example.secondtasksolution.view.episode3.NavigationToNavigationActivi
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
